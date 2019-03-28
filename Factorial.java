0b52c68907d94e0393dc8f93039526f0e6dd95d0
Nadeem Ahmad <nibi0629@in.uni-weimar.de>
2019-03-28 09:45:31 +0100

    updated

--- Factorial.java
+++ Factorial.java
@@ -1,4 +1,32 @@
+import java.util.Scanner;
 
 public class Factorial {
-	//todo: implement a recursive functions whicht gives you the factorial value of a given number
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter valid number");
+		 int n = scan.nextInt();
+	      
+		int r= Factorial(n);
+		System.out.println(r);
+	}
+	
+	public static int  Factorial(int n) {
+		// input n
+		// n is decreasing to 0
+		// until n > 1 continue
+		// if n == 1 stop
+		if( n > 1 ) {
+			// basic case : continue
+			return n*Factorial(n-1);
+		} else {
+			// stop case
+			return 1;
+		}
+	}
+
 }
+
--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,49 @@
+import java.util.Scanner;
 
 public class Fibonacci {
-	//todo: The Fibonacci sequence is a sequence where the current number is the sum of the previous numbers. You can read it up here:
-	//https://en.wikipedia.org/wiki/Fibonacci_number
-	//Write an iterative method fib which takes an integer as parameter. This integer corresponds to the position in the Fibonacci sequence.
-	//fib should return the fibonacci number at this position. For example, fib(4) should return 3.
-	//Now, write a second methond fibRek. fibRek does exactly the same like fib, but it works recursive.
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("enter valid number");
+	 int n = scan.nextInt();
+      
+	int r= Fiborek(n);
+	int s=fiboiter(n);
+	System.out.println(r);
+	System.out.println(s);
+}
+
+public static int  Fiborek(int n) {
+	// input n
+	// n is decreasing to 0
+	// until n > 1 continue
+	// if n == 1 stop
+	if( n < 2 ) {
+		// basic case : continue
+		return n;
+	} else {
+		// stop case
+		return Fiborek(n-1)+Fiborek(n-2);
+	}
+	
+}
+
+public static int fiboiter(int n) {
+	
+	int a = 0; 
+	int b = 0;
+	int c = 1;
+	if(n<2) {
+		a=n;
+	}
+	else {
+		for(int i =1; i<n;i++ ) {
+			a=b+c;
+			int temp = c;
+		    c=a;
+			b=temp;
+		}
+	}
+	return a;
+}
+	
 }
